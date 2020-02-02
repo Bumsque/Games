ec7cf96178c85dd524bb8ac444320e6ae67ade8d
Bumsque <38937657+Bumsque@users.noreply.github.com>
2020-02-02 18:09:51 +0100

    Add files via upload

---
+++ SceneLoader.cs
@@ -0,0 +1,179 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public void MenuLoader()
+    {
+        SceneManager.LoadScene("Mainmenu");
+    }
+    public void PlayLoader()
+    {
+        SceneManager.LoadScene("PLAY");
+
+    }
+    public void quest2Loader()
+    {
+        SceneManager.LoadScene("Question 2");
+
+    }
+    public void quest3Loader()
+    {
+        SceneManager.LoadScene("Question 3");
+
+    }
+    public void quest4Loader()
+    {
+        SceneManager.LoadScene("Question 4");
+
+    }
+    public void quest5Loader()
+    {
+        SceneManager.LoadScene("Question 5");
+
+    }
+    public void quest6Loader()
+    {
+        SceneManager.LoadScene("Question 6");
+
+    }
+    public void quest7Loader()
+    {
+        SceneManager.LoadScene("Question 7");
+
+    }
+    public void quest8Loader()
+    {
+        SceneManager.LoadScene("Question 8");
+
+    }
+    public void quest9Loader()
+    {
+        SceneManager.LoadScene("Question 9");
+    }
+    public void quest10Loader()
+    {
+        SceneManager.LoadScene("Question 10");
+
+    }
+    public void quest11Loader()
+    {
+        SceneManager.LoadScene("Question 11");
+    }
+    public void quest12Loader()
+    {
+        SceneManager.LoadScene("Question 12");
+    }
+    public void quest13Loader()
+    {
+        SceneManager.LoadScene("Question 13");
+    }
+    public void quest14Loader()
+    {
+        SceneManager.LoadScene("Question 14");
+    }
+    public void quest15Loader()
+    {
+        SceneManager.LoadScene("Question 15");
+    }
+    public void quest16Loader()
+    {
+        SceneManager.LoadScene("Question 16");
+    }
+    public void quest17Loader()
+    {
+        SceneManager.LoadScene("Question 17");
+    }
+    public void correctLoader()
+    {
+        SceneManager.LoadScene("correct 1");
+
+    }
+    public void WrongLoader()
+    {
+        SceneManager.LoadScene("wrong");
+
+    }
+    public void correct2Loader()
+    {
+        SceneManager.LoadScene("correct 2");
+
+    }
+    public void correct3Loader()
+    {
+        SceneManager.LoadScene("correct 3");
+
+    }
+    public void correct4Loader()
+    {
+        SceneManager.LoadScene("correct 4");
+
+    }
+    public void correct5Loader()
+    {
+        SceneManager.LoadScene("correct 5");
+    }
+    public void correct6Loader()
+    {
+        SceneManager.LoadScene("correct 6");
+    }
+    public void correct7Loader()
+    {
+        SceneManager.LoadScene("correct 7");
+    }
+    public void correct8Loader()
+    {
+        SceneManager.LoadScene("correct 8");
+    }
+    public void correct9Loader()
+    {
+        SceneManager.LoadScene("correct 9");
+    }
+    public void correct10Loader()
+    {
+        SceneManager.LoadScene("correct 10");
+    }
+    public void correct11Loader()
+    {
+        SceneManager.LoadScene("correct 11");
+    }
+    public void correct12Loader()
+    {
+        SceneManager.LoadScene("correct 12");
+    }
+    public void correct13Loader()
+    {
+        SceneManager.LoadScene("correct 13");
+    }
+    public void correct14Loader()
+    {
+        SceneManager.LoadScene("correct 14");
+    }
+    public void correct15Loader()
+    {
+        SceneManager.LoadScene("correct 15");
+    }
+    public void correct16Loader()
+    {
+        SceneManager.LoadScene("correct 16");
+    }
+    public void correct17Loader()
+    {
+        SceneManager.LoadScene("correct 17");
+    }
+    public void HelpLoader()
+    {
+        SceneManager.LoadScene("LASSA HELP");
+
+    }
+    public void ExitLoader()
+    {
+        SceneManager.LoadScene("QUIT");
+    }
+    public void Quit()
+    {
+        Application.Quit();
+    }
+}
